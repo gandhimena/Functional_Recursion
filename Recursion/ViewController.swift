17dc7341f328bc9d55628e25ee030c9f897672dd
gandhi <gandhimena@gmail.com>
2018-01-27 13:31:55 -0600

    first commit

--- Recursion/ViewController.swift
+++ Recursion/ViewController.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
+// indirect le indica al compilador que el tipo es recursivo
+// tipo recursivo: es aquiel tipo que se referencia a si mismo en su definición.
+
+
+indirect enum List<T>{
+	case empty
+	case content(head:T, tail: List<T>)
+}
+
 
 class ViewController: UIViewController {
 
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	//mala recursión, por la pila de momoria.
+	func totalCost(listOf items: List<Int>) -> Int{
+		switch items {
+		case .empty:
+			return 0
+		case .content(let head, let tail):
+			return head + totalCost(listOf: tail)
+		}
 	}
+	
+	//APS(Acumulator Passing Style)
+	func totalCostAccumulator(listOf items: List<Int>) -> Int{
+		func totalCostAccumulator(listOf items: List<Int>, accumulator: Int) -> Int{
+			switch items {
+			case .empty:
+				return accumulator
+			case .content(let head, let tail):
+				return totalCostAccumulator(listOf: tail, accumulator: head + accumulator)
+			}
+		}
+		return totalCostAccumulator(listOf: items, accumulator: 0)
+	}
+	
+	
+	
 
 
 }
